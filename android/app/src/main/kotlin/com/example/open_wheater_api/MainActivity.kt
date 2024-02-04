53958af9705b8b36407744c7e3a19e4e07ecaa81
Pedro Victor <92291111+pedrovictormotasilva@users.noreply.github.com>
2024-02-04 12:10:29 -0300

    project fineshed

---
+++ android/app/src/main/kotlin/com/example/open_wheater_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.open_wheater_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/my_data.dart
@@ -0,0 +1,1 @@
+String API_KEY = "b2839085eb269b66aa055eac0880df8d";
